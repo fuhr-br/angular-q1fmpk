c1b1783ab45c91307f148e4dacfa189c438662e9
fuhr-br <andersonfuhr@yahoo.com.br>
2021-10-29 10:46:36 -0300

    add modulo cart

--- src/app/products.ts
+++ src/app/products.ts
@@ -10,22 +10,21 @@ export const products = [
     id: 1,
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'Um grande telefone com uma das melhores telas',
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'Um grande telefone com uma das melhores cameras',
   },
   {
     id: 3,
     name: 'Phone Standard',
     price: 299,
-    description: ''
-  }
+    description: '',
+  },
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
